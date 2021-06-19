9c20a262f4dc144c1cc0bb39c006966aa4968b0a
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2021-06-19 12:52:20 -0400

    Combine multiple config sources

--- src/main/scala/info/ditrapani/Main.scala
+++ src/main/scala/info/ditrapani/Main.scala
@@ -2,29 +2,45 @@ package info.ditrapani
 
 import zio.config.magnolia.DeriveConfigDescriptor.descriptor
 import com.typesafe.config.ConfigFactory
-import zio.{ExitCode, Has, Layer, URIO, ZEnv, ZIO}
-import zio.config.typesafe.TypesafeConfig
+import zio.{ExitCode, Has, IO, URIO, ZEnv, ZIO, ZLayer}
+import zio.config.typesafe.TypesafeConfigSource
 import zio.console.{Console, putStrLn}
-import zio.config.{ConfigDescriptor, ReadError, getConfig}
+import zio.config.{ConfigDescriptor, ConfigSource, ReadError, read}
 
 import java.net.URI
 
 object Main extends zio.App {
   override def run(args: List[String]): URIO[ZEnv, ExitCode] =
-    app.provideCustomLayer(configLayer).exitCode
+    app.exitCode
+    // app.provideCustomLayer(configLayer).exitCode
 
-  val app: ZIO[Console with Has[Config], Throwable, Unit] =
+  val app: ZIO[Console, Throwable, Unit] =
     for {
-      config <- getConfig[Config]
-      _ <- putStrLn(config.toString())
+      // config <- getConfig[Config]
+      // _ <- putStrLn(config.toString())
+      _ <- putStrLn("config.toString()")
     } yield ()
 
-  val configLayer: Layer[ReadError[String], Has[Config]] = {
+  /*
+  val configLayer: ZLayer[ZEnv, ReadError[String], Has[Config]] = {
+    loadConfig.map(configDescriptor => {
+      val x: Either[ReadError[String], Config] = read[Config](configDescriptor )
+      x
+    }).either.toLayer
+  }
+
+   */
+
+  val loadConfig: ZIO[ZEnv, ReadError[String], ConfigDescriptor[Config]] = {
     val configDescriptor: ConfigDescriptor[Config] = descriptor[Config]
-    TypesafeConfig.fromTypesafeConfig[Config](
-      ConfigFactory.defaultApplication(),
-      configDescriptor,
-    )
+    for {
+      envConfigSource <- ConfigSource.fromSystemEnv(keyDelimiter = Some('_'), valueDelimiter = Some(','))
+      typesafeConfigSource <- IO.fromEither(TypesafeConfigSource.fromTypesafeConfig(ConfigFactory.defaultApplication()))
+      sources: ConfigSource = envConfigSource <> typesafeConfigSource
+    } yield {
+      // configDescriptor.from(sources)
+      configDescriptor.updateSource(_ => sources)
+    }
   }
 }
 
